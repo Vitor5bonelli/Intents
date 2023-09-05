ef97c245fcc6a5f8343645f8d42c76a61c5e542d
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-09-05 20:22:50 -0300

    class 05/09/2023
    Intents

---
+++ aula05092023/app/src/main/java/com/example/aula05092023/ParametroActivity.kt
@@ -0,0 +1,16 @@
+package com.example.aula05092023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aula05092023.databinding.ActivityMainBinding
+
+class ParametroActivity : AppCompatActivity() {
+    private val amb : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+    }
+}
