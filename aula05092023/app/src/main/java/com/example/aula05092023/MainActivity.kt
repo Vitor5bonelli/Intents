195943a15abaea36fa58831b30825b7e978e4c59
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-09-05 21:55:18 -0300

    class 05/09/2023 final

--- aula05092023/app/src/main/java/com/example/aula05092023/MainActivity.kt
+++ aula05092023/app/src/main/java/com/example/aula05092023/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aula05092023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.aula05092023.databinding.ActivityMainBinding
@@ -9,8 +10,33 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    companion object {
+        const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
+        const val PARAMETRO_REQUEST_CODE = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+        supportActionBar?.subtitle = "Main Activity"
+
+        amb.entrarParametroBt.setOnClickListener {
+            val parametroIntent: Intent = Intent(this, ParametroActivity::class.java)
+            //val parametrosBundle = Bundle()
+            parametroIntent.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
+
+            startActivityForResult(parametroIntent, PARAMETRO_REQUEST_CODE)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PARAMETRO_REQUEST_CODE && resultCode == RESULT_OK){
+            data?.getStringExtra(PARAMETRO_EXTRA)?.let {parametro ->
+                amb.parametroTv.text = parametro
+            }
+        }
+    }
+
 }
--- aula05092023/app/src/main/java/com/example/aula05092023/ParametroActivity.kt
+++ aula05092023/app/src/main/java/com/example/aula05092023/ParametroActivity.kt
@@ -1,16 +1,35 @@
 package com.example.aula05092023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.aula05092023.databinding.ActivityMainBinding
+import com.example.aula05092023.databinding.ActivityParametroBinding
 
 class ParametroActivity : AppCompatActivity() {
-    private val amb : ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+    private val apb by lazy {
+        ActivityParametroBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(amb.root)
+        setContentView(apb.root)
+        supportActionBar?.subtitle = "ParametroActivity"
+
+        val parametroRecebido : String = intent.getStringExtra(MainActivity.PARAMETRO_EXTRA) ?: ""
+        apb.parametroEt.setText(parametroRecebido)
+
+        intent.getStringExtra(MainActivity.PARAMETRO_EXTRA)?.let{ parametro ->
+            apb.parametroEt.setText(parametro)
+        }
+
+        val parametroRetorno = apb.parametroEt.text.toString()
+
+        apb.enviarParametroBt.setOnClickListener {
+            val intentRetorno = Intent()
+            intentRetorno.putExtra(MainActivity.PARAMETRO_EXTRA, apb.parametroEt.text.toString())
+            setResult(RESULT_OK, intentRetorno)
+            finish()
+        }
     }
 }
